dda04bcd0e9ae97bb369f3b52ee27074222d216b
Ajith Kattu <ajithkt@gmail.com>
2022-01-05 08:03:15 -0500

    Almost completed

--- database/models/album.js
+++ database/models/album.js
@@ -1,1 +1,17 @@
 // Todo: create Album Schema
+// Todo: Create Artist Model
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const SchemaTypes = mongoose.Schema.Types;
+
+const AlbumSchema = new Schema({
+  title: String,
+  date: Date,
+  copiesSold: Number,
+  numberTracks: Number,
+  image: String,
+  revenue: Number,
+  //   revenue: SchemaTypes.Double,
+});
+
+module.exports = AlbumSchema;
--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -1,9 +1,33 @@
-const Artist = require('../models/artist');
+const Artist = require("../models/artist");
 
 /**
  * Finds the lowest and highest age of artists in the Artist collection
  * @return {promise} A promise that resolves with an object
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
+
+// let minage = minage();
+// let maxage = maxage();
+// console.log(minage, maxage);
+
 module.exports = () => {
+  const minQuery = Artist.find({})
+    .sort({ age: 1 })
+    .limit(1)
+    .then((artist) => artist[0].age);
+
+  const maxQuery = Artist.find({})
+    .sort({ age: -1 })
+    .limit(1)
+    .then((artist) => artist[0].age);
+
+  return Promise.all([minQuery, maxQuery]).then((result) => {
+    return { min: result[0], max: result[1] };
+  });
 };
+
+// function minage() {}
+
+// function maxage() {
+//   Artist.find({}).sort({ age: -1 }).limit(1);
+// }
